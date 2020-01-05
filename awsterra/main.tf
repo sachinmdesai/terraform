b33ac26068a2a041412c755132a64e4ee6fdfc8e
sachinmdesai <46660552+sachinmdesai@users.noreply.github.com>
2020-01-05 14:50:42 -0500

    Update main.tf

--- awsterra/main.tf
+++ awsterra/main.tf
@@ -8,3 +8,10 @@ module "storage" {
     source = "./storage"
     project_name = "${var.project_name}"
 }
+
+module "networking" {
+    source = "./networking"
+    vpc_cidr = "${var.vpc_cidr}"
+    public_cidrs = "${var.public_cidrs}"
+    accessip = "${var.accessip}"
+}
