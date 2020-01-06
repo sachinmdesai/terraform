b70f58dce4c82215585827f8befb341f18687b2c
EC2 Default User <ec2-user@ip-10-0-0-8.ec2.internal>
2020-01-06 02:33:39 +0000

    x

--- awsterra/compute/main.tf
+++ awsterra/compute/main.tf
@@ -15,3 +15,15 @@ resource "aws_key_pair" "tf_auth" {
     key_name = "${var.key_name}"
     public_key = "${file(var.public_key_path)}"
 }
+
+resource "aws_instance" "tf_server" {
+    count = "${var.instance_count}"
+    instance_type = "${var.instance_type}"
+    ami = "${data.aws_ami.server_ami.id}"
+    tags {
+        Name = "tf_server_public_${count.index +1}"
+    }
+    key_name = "${aws_key_pair.tf_auth.id}"
+    vpc_security_group_id = "[${var.security_group}]"
+    subnet_id = "${element(var.subnet , count.index)}"
+}
