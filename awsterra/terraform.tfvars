e6c74d4aa2aa6c06d84d3913926fb26edb17ea20
EC2 Default User <ec2-user@ip-10-0-0-198.ec2.internal>
2020-01-06 06:36:47 +0000

    x

--- awsterra/terraform.tfvars
+++ awsterra/terraform.tfvars
@@ -3,3 +3,7 @@ project_name = "la-terraform"
 vpc_cidr = "10.123.0.0/16"
 public_cidrs = ["10.123.1.0/24","10.123.2.0/24"]
 accessip = "0.0.0.0/0"
+key_name = "tfkey"
+public_key_path = "/home/ec2-user/.ssh/idpub"
+server_instance_type = "t2.micro"
+instance_count = 2
--- awsterra/variables.tf
+++ awsterra/variables.tf
@@ -1,7 +1,23 @@
 variable "aws_region" {}
+
+#---------------storage--------------
 variable "project_name" {}
+
+#--------------networking------------
 variable "vpc_cidr" {}
 variable "public_cidrs"{
     type = "list"
 }
 variable "accessip" {}
+
+
+#----------------compute-------------
+
+variable "key_name" {}
+
+variable "public_key_path" {}
+
+variable "instance_count" {}
+
+variable "server_instance_type" {}
+
